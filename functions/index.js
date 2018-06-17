3959272183c101f1d6dcb416046414a107ec6b32
Lemmah <lemayiannakolah@gmail.com>
2018-06-18 02:12:27 +0300

    add endpoint to get items

--- functions/index.js
+++ functions/index.js
@@ -10,34 +10,44 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from a Severless Database!");
 });
 
+const getItemsFromDatabase = (res) => {
+  let items = [];
+
+  return database.on('value', (snapshot) => {
+    snapshot.forEach((item) => {
+      items.push({
+        id: item.key,
+        item: item.val().item
+      });
+    });   
+    res.status(200).json(items);
+  }, (error) => {
+    res.status(error.code).json({
+      message: `Something went wrong. ${error.message}`
+    })
+  })
+};
+
 exports.addItem = functions.https.onRequest((req, res) => {
   return cors(req, res, () => {
     if(req.method !== 'POST') {
       return res.status(401).json({
         message: 'Not allowed'
       })
-    }
-    console.log(req.body)
-  
-    const item = req.body.item
-
+    };
+    const item = req.body.item;
     database.push({ item });
+    getItemsFromDatabase(res)
+  });
+});
 
-    let items = [];
-
-    return database.on('value', (snapshot) => {
-      snapshot.forEach((item) => {
-        items.push({
-          id: item.key,
-          items: item.val().item
-        });
+exports.getItems = functions.https.onRequest((req, res) => {
+  return cors(req, res, () => {
+    if(req.method !== 'GET') {
+      return res.status(401).json({
+        message: 'Not allowed'
       });
-      
-      res.status(200).json(items)
-    }, (error) => {
-      res.status(error.code).json({
-        message: `Something went wrong. ${error.message}`
-      })
-    })
-  })
-})
+    };
+    getItemsFromDatabase(res)
+  });
+});
